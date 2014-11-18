f59cb8e3b025658cdd963e98b5e2595e1517e8e9
Jerome Heymonet <jerome.heymonet@gmail.com>
2014-11-18 14:11:09 +0100

    test iOS version for animation

--- LGSwipeableTableViewCell.m
+++ LGSwipeableTableViewCell.m
@@ -243,9 +243,11 @@
          } completion:nil];
          */
         // iOS 7 only
-        [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.4 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseIn animations:^{
-            [_scrollView setContentOffset:expanded?_offsetExpanded:_offsetCollapsed animated:NO];
-        } completion:nil];
+        if (floor(NSFoundationVersionNumber) >= NSFoundationVersionNumber_iOS_7_0) {
+            [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.4 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseIn animations:^{
+                [_scrollView setContentOffset:expanded?_offsetExpanded:_offsetCollapsed animated:NO];
+            } completion:nil];
+        }
     } else {
         [UIView animateWithDuration:0.2
                               delay:0
